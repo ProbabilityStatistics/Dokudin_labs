1e16f158cf6499c35165bdeb296db84c2cdc8321
Nikita <NikDok05@gmail.com>
2026-01-07 19:18:51 +0300

    (develop) The filtering and editing functions have been rewritten

--- lab1/process.cpp
+++ lab1/process.cpp
@@ -178,45 +178,65 @@ bool check_by_occupancy(const CS &s, const double param) {
 	return s.get_percent() == param;
 }
 
-int choose_filter(int struct_type) {
-    std::string str;
-    int filter, ans = 0;
-    std::cout << "Enter: " << std::endl
+std::vector<int> filtration(std::unordered_map<int, Pipe> &p, std::unordered_map<int, CS> &cs, int struct_type) {
+    std::vector<int> ids;
+    if (!struct_type) {
+        std::cout << "Choose struct: " << std::endl
+            << "1. Pipe" << std::endl
+            << "2. CS" << std::endl;
+        struct_type = get_correct_number<int>(1, 2);
+    }
+    std::cout << "Filter by: " << std::endl
         << "1. Filter by name" << std::endl
         << "2. Filter by parameter" << std::endl;
-    filter = get_correct_number<int>(1, 2);
-    if (struct_type == 1) ans = filter;
-    else if (struct_type == 2) ans = filter + 2;
-    return ans;
+    int filter_choice = get_correct_number<int>(1, 2);
+    if (struct_type == 1) {
+        if (filter_choice == 1) {
+            std::string name;
+            std::cout << "Enter name: ";
+            std::cin >> name;
+            ids = find_by_filter(p, check_by_name<Pipe>, name);
+        } else {
+            bool repair_status;
+            std::cout << "Enter repair status (0 or 1): ";
+            repair_status = get_correct_number<bool>(0, 1);
+            ids = find_by_filter(p, check_by_repair, repair_status);
+        }
+    } else if (struct_type == 2) {
+        if (filter_choice == 1) {
+            std::string name;
+            std::cout << "Enter name: ";
+            std::cin >> name;
+            ids = find_by_filter(cs, check_by_name<CS>, name);
+        } else {
+            double percent;
+            std::cout << "Enter the percentage of occupancy: ";
+            percent = get_correct_number<double>(0, 100.0);
+            ids = find_by_filter(cs, check_by_occupancy, percent);
+        }
+    } else {
+        std::cout << "Error! Incorrect struct type" << std::endl;
+    }
+    return ids;
 }
 
-void filtration(std::unordered_map<int, Pipe> &p, std::unordered_map<int, CS> &cs) {
-    std::cout << "Enter struct: " << std::endl
+void filtration_void_wrapper(std::unordered_map<int, Pipe> &p, std::unordered_map<int, CS> &cs) {
+    std::cout << "Choose struct: " << std::endl
         << "1. Pipe" << std::endl
         << "2. CS" << std::endl;
     int struct_type = get_correct_number<int>(1, 2);
-    int filter_num = choose_filter(struct_type);
-    std::variant<std::string, bool, double> param;
-    if (filter_num == 1 || filter_num == 3) {
-        std::cout << "Enter struct name: " << std::endl;
-        param = get_str();
-    } else if (filter_num == 2) {
-        std::cout << "Enter repair status (0. under repair, 1. repaired)" << std::endl;
-        param = get_correct_number<bool>(0, 1);
+    auto ids = filtration(p, cs, struct_type);
+    if (struct_type == 1) {
+        for (int id : ids) {
+            if (p.contains(id))
+                std::cout << p[id];
+        }
     } else {
-        std::cout << "Enter percent" << std::endl;
-        param = get_correct_number<double>(0.0, 100.0);
+        for (int id : ids) {
+            if (cs.contains(id))
+                std::cout << cs[id];
+        }
     }
-    if (struct_type == 1)
-        std::visit([&p, &filter_num](auto&& arg) {
-            if (filter_num == 2) find_by_filter(p, check_by_repair, arg);
-            else find_by_filter(p, check_by_name, arg);
-        }, param);
-    else 
-        std::visit([&cs, &filter_num](auto&& arg) {
-            if (filter_num == 4) find_by_filter(cs, check_by_occupancy, arg);
-            else find_by_filter(cs, check_by_name, arg);
-        }, param);
     return;
 }
 
@@ -233,98 +253,61 @@ std::vector<int> enter_id() {
 }
 
 void pipe_edit(std::unordered_map<int, Pipe> &P, std::vector<int> &ids) {
-    for (int i = 0; i < (int) ids.size(); i++) {
-        if (P.contains(ids[i])) {
-            P[ids[i]].repair_change();
-        }
-    }
-    return;
-}
-
-void pipe_delete(std::unordered_map<int, Pipe> &P, std::vector<int> &ids) {
-    for (int i = 0; i < (int) ids.size(); i++) {
-        if (P.contains(ids[i])) {
-            P.erase(ids[i]);
+    for (int id : ids) {
+        if (P.contains(id)) {
+            P[id].repair_change();
         }
     }
     return;
 }
 
 void cs_edit(std::unordered_map<int, CS> &comp_st, std::vector<int> &ids, bool mode) {
-    for (int i = 0; i < (int) ids.size(); i++) {
-        if (comp_st.contains(ids[i])) {
-            comp_st[ids[i]].mode_change(mode);
-        }
-    }
-    return;
-}
-
-void cs_delete(std::unordered_map<int, CS> &comp_st, std::vector<int> &ids) {
-    for (int i = 0; i < (int) ids.size(); i++) {
-        if (comp_st.contains(ids[i])) {
-            comp_st.erase(ids[i]);
+    for (int id : ids) {
+        if (comp_st.contains(id)) {
+            comp_st[id].mode_change(mode);
         }
     }
     return;
 }
 
 void edit(std::unordered_map<int, Pipe> &P, std::unordered_map<int, CS> &comp_st) {
-    int struct_type;
-    std::cout << "Enter:" << std::endl
-        << "1. Pipe" << std::endl
-        << "2. CS" << std::endl;
-    struct_type = get_correct_number<int>(1, 2);
+    std::vector<int> ids;
     std::cout << "Enter:" << std::endl
         << "1. Sort by filter" << std::endl
         << "2. Sort by IDs" << std::endl;
     int type = get_correct_number<int>(1, 2);
-    std::vector<int> ids;
-    if (type == 1) {
-        int filter_num = choose_filter(struct_type);
-        std::variant<std::string, bool, double> param;
-        if (filter_num == 1 || filter_num == 3) {
-            std::cout << "Enter struct name: " << std::endl;
-            param = get_str();
-        } else if (filter_num == 2) {
-            std::cout << "Enter repair status (0. under repair, 1. repaired)" << std::endl;
-            param = get_correct_number<bool>(0, 1);
-        } else {
-            std::cout << "Enter percent" << std::endl;
-            param = get_correct_number<double>(0.0, 100.0);
-        }
-        if (struct_type == 1) 
-            std::visit([&P, &filter_num, &ids](auto&& arg) {
-                if (filter_num == 2) ids = find_by_filter(P, check_by_repair, arg);
-                else ids = find_by_filter(P, check_by_name, arg);
-            }, param);
-        else 
-            std::visit([&cs, &filter_num, &ids](auto&& arg) {
-                if (filter_num == 4) ids = find_by_filter(comp_st, check_by_occupancy, arg);
-                else ids = find_by_filter(comp_st, check_by_name, arg);
-            }, param);
-    } else{
+    std::cout << "Choose struct: " << std::endl
+        << "1. Pipe" << std::endl
+        << "2. CS" << std::endl;
+    int struct_type = get_correct_number<int>(1, 2);
+    if (type == 1)
+        ids = filtration(P, comp_st, struct_type);
+    else
         ids = enter_id();
-    }
+    
     std::cout << "Enter:" << std::endl
         << "1. Change" << std::endl
         << "2. Delete" << std::endl;
     int mode = get_correct_number<int>(1, 2);
-    if (mode == 1 && !ids.empty() && struct_type == 1) {
-        pipe_edit(P, ids);
-    } else if (mode == 1 && !ids.empty() && struct_type == 2) {
-        bool mode;
-        std::cout << "Enter:" << std::endl
-            << "0. Turn off" << std::endl
-            << "1. Turn on" << std::endl;
-        mode = get_correct_number<bool>(0, 1);
-        if (mode == 1 || mode == 0) cs_edit(comp_st, ids, mode);
-        else std::cout << "Error! Incorrect mode, returning to main menu" << std::endl;
-    } else if (mode == 2 && !ids.empty() && struct_type == 1) {
-        pipe_delete(P, ids);
-    } else if (mode == 2 && !ids.empty() && struct_type == 2) {
-        cs_delete(comp_st, ids);
+    if (struct_type == 1) {
+        if (mode == 1) {
+            pipe_edit(P, ids);
+            std::cout << "Pipes edited" << std::endl;
+        } else {
+            delete_by_ids(P, ids);
+            std::cout << "Pipes deleted" << std::endl;
+        }
     } else {
-        std::cout << "Error! Incorrect input, returning to main menu" << std::endl;
+        if (mode == 1) {
+            std::cout << "Enter mode (1 to increase, 0 to decrease): ";
+            bool mode;
+            mode = get_correct_number<bool>(0, 1);
+            cs_edit(comp_st, ids, mode);
+            std::cout << "CSs edited" << std::endl;
+        } else {
+            delete_by_ids(comp_st, ids);
+            std::cout << "CSs deleted" << std::endl;
+        }
     }
     return;
 }
@@ -344,7 +327,7 @@ void process() {
         {[&comp_st]() {cs_add(comp_st);}, "Create CS"},
         {[&comp_st]() {cs_info(comp_st);}, "Print CS info"},
         {[&comp_st]() {execute_mode_change(comp_st);}, "Change CS working workshops"},
-        {[&P, &comp_st]() {filtration(P, comp_st);}, "Find"},
+        {[&P, &comp_st]() {filtration_void_wrapper(P, comp_st);}, "Filter"},
         {[&P, &comp_st]() {edit(P, comp_st);}, "Edit"},
         {[&P, &comp_st]() {save_into_file(P, comp_st);}, "Save"},
         {[&P, &comp_st]() {read_from_file(P, comp_st);}, "Load"},
